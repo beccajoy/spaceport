6fe918470de03437b718e1e73da8d0e6a31c1149
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-03-19 21:57:00 -0600

    Complete Iteration 0 & 1

--- src/being.js
+++ src/being.js
@@ -1,4 +1,13 @@
 class Being {
-}
+  constructor(name, species, credits){
+    this.isAlive = true;
+    this.name = name;
+    this.species = species;
+    this.credits = 0;
+  }
+  updateCredits(newCredits) {
+    this.credits = this.credits + newCredits;
+  };
+};
 
 module.exports = Being;
